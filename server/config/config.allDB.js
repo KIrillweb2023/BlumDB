79b6b317c81ac0c3dedfecaadd450d588116add2
Kirill <129139006+KIrillweb2023@users.noreply.github.com>
2024-08-30 10:05:48 +0300

    Update config.allDB.js

--- server/config/config.allDB.js
+++ server/config/config.allDB.js
@@ -7,12 +7,21 @@ export const configAllDb = () => {
         }
 
         files.forEach((file) => {
-            fs.readFile(`./Databases/${file}`, 'utf8', (err, data) => {
+           fs.readFile(`./Databases/${file}`, 'utf8', (err, data) => {
                 if(err){
                     throw new Error(err)
                 }
 
+                fs.stat(`./Databases/${file}`, (err, status) => {
+                    if(err) {
+                        throw new Error("Не удалось посчитать размер файла:", err)
+                    }
+                    
+                    console.log(`Size file in bytes:`, status.size)
+                })
+
                 const listTextFile = JSON.parse(data)
+               
                 console.log(listTextFile)
             })
         })
